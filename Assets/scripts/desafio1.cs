103134fcd1f24f771d543b75e5e0f2d8051abb46
MARCELO DA SILVA EVANGELISTA VILA NOVA <46058880807@info.local>
2024-11-04 15:46:00 -0300

    desafio 1 com comentario

--- Assets/scripts/desafio1.cs
+++ Assets/scripts/desafio1.cs
@@ -3,21 +3,25 @@ using UnityEngine;
 public class desafio1 : MonoBehaviour
 {
     [SerializeField] int power_up = 0;
-    [SerializeField] int vidaheroi = 100;
+    [SerializeField] int vidaheroiI = 100;
+    [SerializeField] int vidaheroiA = 100;
     int valormorte = 0;
 
     string voltadosmortos = "Ainda não, vida atual = ";
     string fim = "Game over";
-    int vida2 = 100 / 2;
+   
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the MonoBehaviour is create
+
     void Start()
     {
-     if (power_up > 0 && vidaheroi == valormorte) 
+        int vida2 = vidaheroiI / 2;
+
+        if (power_up > 0 && vidaheroiA == valormorte) 
         {
             print(voltadosmortos + vida2);
         } 
-        else if (power_up <= 0 && vidaheroi == valormorte)
+        else if (power_up <= 0 && vidaheroiA == valormorte)
         {
             print(fim);
         }
